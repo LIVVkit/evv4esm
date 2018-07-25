b76e09c287a35a977627dd5cbfcfa02f80703619
Joseph H Kennedy <kennedyjh@ornl.gov>
2018-07-25 11:54:35 -0400

    Update licensing for release

--- eve/__main__.py
+++ eve/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Copyright (c) 2015,2016, UT-BATTELLE, LLC
+# coding=utf-8
+# Copyright (c) 2018 UT-BATTELLE, LLC
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
