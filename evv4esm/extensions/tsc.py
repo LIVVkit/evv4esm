7f1db8f7d117a665ffb7719ce1d0235ed0445d98
Michael Kelleher <kelleher.mike@gmail.com>
2021-05-07 07:43:30 -0700

    Remove ref file pattern Since both ref and test files will match the same pattern, use one string to match both

--- evv4esm/extensions/tsc.py
+++ evv4esm/extensions/tsc.py
@@ -163,14 +163,13 @@ def main(args):
         args.test_case += '1'
         args.ref_case += '2'
 
-    file_search_glob_ref = '{d}/*eam_????.h0.0001-01-01-?????.nc.{s}'
     file_search_glob = '{d}/*eam_????.h0.0001-01-01-?????.nc.{s}'
     truth_ens = {instance: list(files) for instance, files in groupby(
-            sorted(glob.glob(file_search_glob_ref.format(d=args.ref_dir, s='DT0001'))),
+            sorted(glob.glob(file_search_glob.format(d=args.ref_dir, s='DT0001'))),
             key=lambda f: e3sm.component_file_instance('eam', f))}
 
     ref_ens = {instance: list(files) for instance, files in groupby(
-            sorted(glob.glob(file_search_glob_ref.format(d=args.ref_dir, s='DT0002'))),
+            sorted(glob.glob(file_search_glob.format(d=args.ref_dir, s='DT0002'))),
             key=lambda f: e3sm.component_file_instance('eam', f))}
 
     test_ens = {instance: list(files) for instance, files in groupby(
