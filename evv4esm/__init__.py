93b01ab88bc91a29b74aae2760c8ce0d480553f3
Mike Kelleher <1476361+mkstratos@users.noreply.github.com>
2022-03-07 10:00:52 -0500

    Fix TSC test passing criteria (#9)
    * Fix TSC test to match original pass/fail method
    Per E3SM issue 4759 (https://github.com/E3SM-Project/E3SM/issues/4759)
    The TSC test should fail only if all points between 300s - 600s are
    below the failure threshold (p_threshold). This also fixes the plotting
    of P_min so that the variable threshold is plotted rather than a fixed
    0.5%
    
    * Update copyright date
    
    * Bump version number
    
    * Fix readme formatting

--- evv4esm/__init__.py
+++ evv4esm/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright (c) 2018-2021 UT-BATTELLE, LLC
+# Copyright (c) 2018-2022 UT-BATTELLE, LLC
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
